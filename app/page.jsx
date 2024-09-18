97d8f30fbb73e16af69521cb3b541b021037b813
shamimbinzahid-repliq <shamimbinzahid@repliq.dev>
2024-09-18 13:55:47 +0600

    individual section decoupled as functions

--- app/page.jsx
+++ app/page.jsx
@@ -24,6 +24,91 @@ function Loading() {
   );
 }
 
+function IntroSection({ data }) {
+  return (
+    <div className="relative flex flex-col justify-start sm:flex-row sm:items-center gap-4 p-4 rounded-tr-[64px] rounded-br-[64px] rounded-tl-[100px] rounded-bl-[100px]">
+      <div className="flex w-full sm:w-fit justify-between items-start">
+        <Image
+          alt="shamimbinzahid"
+          src="/images/shameme.webp"
+          id="shameme"
+          height={512}
+          width={512}
+          className="w-32 aspect-1 rounded-full border border-gray-200 dark:border-neutral-700"
+        />
+        <ThemeToggle className="sm:hidden" />
+      </div>
+      <div className="flex flex-col gap-2">
+        <h1 className="text-2xl font-bold text-gray-800 dark:text-neutral-100">
+          {data?.intro.greetings || def.intro.greetings}
+        </h1>
+        <span className="text-base font-normal text-gray-700 dark:text-neutral-200">
+          {data?.intro.tagline || def.intro.tagline}
+        </span>
+        <span className="flex gap-1">
+          <MapPinIcon className="w-4 text-gray-500 dark:text-neutral-400" />
+          <span className="text-sm font-light text-gray-500 dark:text-neutral-400">
+            {data?.intro.location || def.intro.location}
+          </span>
+        </span>
+      </div>
+      <ThemeToggle className="hidden sm:block absolute top-6 right-6" />
+    </div>
+  );
+}
+
+function QuoteSection({ data }) {
+  return (
+    <p className="p-4 text-sm font-medium text-gray-700 dark:text-neutral-200 text-left sm:text-justify">
+      {data?.intro.quote || def.intro.quote}
+    </p>
+  );
+}
+
+function LinksSection({ data }) {
+  return (
+    <div className="text-sm w-full flex flex-col justify-start gap-2 p-4 rounded-xl">
+      <h2 className="font-semibold text-gray-500 dark:text-neutral-400">
+        {data?.links?.label || def?.links?.label}
+      </h2>
+      <div className="w-full grid grid-cols-2 sm:flex sm:flex-wrap gap-2 sm:gap-3 items-start -mx-4">
+        {data?.actions?.show && data?.actions?.resume?.show && (
+          <TestResumeDrawer
+            label={data?.actions?.resume?.label}
+            src={data?.actions?.resume?.url}
+          />
+        )}
+        {data?.actions?.show && data?.actions?.contact?.show && (
+          <TestContactDrawer
+            label={data?.actions?.contact?.label}
+            email={data?.actions?.contact?.address}
+          />
+        )}
+        {(data?.links.values || def.links.values).map((link, index) => (
+          <a
+            key={index}
+            className="group flex items-center w-full sm:w-fit font-semibold text-teal-700 dark:text-teal-500 transition-all px-4 py-3 rounded-full hover:bg-teal-50 dark:hover:bg-teal-950"
+            target="_blank"
+            href={link.url}
+            rel="noopener noreferrer"
+            title={"Open " + link.label}
+          >
+            <ArrowUpRightIcon className="w-0 h-0 group-hover:w-5 group-hover:h-5 transition-all duration-500" />
+            <Image
+              width={20}
+              height={20}
+              alt="Link Icon"
+              src={`/icons/${link.icon}.svg`}
+              className="w-5 h-5 group-hover:w-0 group-hover:h-0 transition-all duration-500"
+            ></Image>
+            <p className="ml-2">{link.label}</p>
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+}
+
 function ExperiencesSection({ data }) {
   return (
     <div className="text-sm w-full flex flex-col justify-start gap-2 p-4 rounded-xl">
@@ -77,6 +162,27 @@ function ExperiencesSection({ data }) {
   );
 }
 
+function AboutSection({ data }) {
+  return (
+    <div className="text-sm flex flex-col justify-start gap-2 p-4 rounded-xl">
+      <h2 className="font-semibold text-gray-500 dark:text-neutral-400">
+        {data?.about.label || def.about.label}
+      </h2>
+      <p className="text-gray-700 dark:text-neutral-200 text text-left sm:text-justify">
+        {data?.about.value || def.about.value}
+      </p>
+    </div>
+  );
+}
+
+function FooterSection({ data }) {
+  return (
+    <h2 className="mx-auto text-sm text-center text-gray-400 dark:text-neutral-400 mt-8 pb-6">
+      2024 ©️ {data?.intro.name || def.intro.name}
+    </h2>
+  );
+}
+
 export default function Home() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -107,116 +213,13 @@ export default function Home() {
 
   return (
     <div className="flex flex-col gap-4 md:justify-center min-h-screen dark:bg-neutral-900 transition-all duration-200 ease-in">
-      {/* Background Pattern */}
-      {/* <Image
-        src="/images/pattern-diam.jpg"
-        width={1920}
-        height={1080}
-        alt="background pattern"
-        className="absolute inset-0 h-screen -z-50 bg-repeat opacity-50 invisible"
-      /> */}
-
-      {/* The main content */}
       <main className="animate-fadeIn flex flex-col gap-2 w-full h-full max-w-5xl p-2 sm:p-6 pt-6 sm:pt-24 mx-auto">
-        {/* Intro section */}
-        <div className="relative flex flex-col justify-start sm:flex-row sm:items-center gap-4 p-4 rounded-tr-[64px] rounded-br-[64px] rounded-tl-[100px] rounded-bl-[100px]">
-          <div className="flex w-full sm:w-fit justify-between items-start">
-            <Image
-              alt="shamimbinzahid"
-              src="/images/shameme.webp"
-              id="shameme"
-              height={512}
-              width={512}
-              className="w-32 aspect-1 rounded-full border border-gray-200 dark:border-neutral-700"
-            />
-            <ThemeToggle className="sm:hidden" />
-          </div>
-          <div className="flex flex-col gap-2">
-            <h1 className="text-2xl font-bold text-gray-800 dark:text-neutral-100">
-              {data?.intro.greetings || def.intro.greetings}
-            </h1>
-            <span className="text-base font-normal text-gray-700 dark:text-neutral-200">
-              {data?.intro.tagline || def.intro.tagline}
-            </span>
-            <span className="flex gap-1">
-              <MapPinIcon className="w-4 text-gray-500 dark:text-neutral-400" />
-              <span className="text-sm font-light text-gray-500 dark:text-neutral-400">
-                {data?.intro.location || def.intro.location}
-              </span>
-            </span>
-          </div>
-          <ThemeToggle className="hidden sm:block absolute top-6 right-6" />
-        </div>
-
-        {/* Quote section */}
-        <p className="p-4 text-sm font-medium text-gray-700 dark:text-neutral-200 text-left sm:text-justify">
-          {data?.intro.quote || def.intro.quote}
-        </p>
-
-        {/* Links section */}
-        <div className="text-sm w-full flex flex-col justify-start gap-2 p-4 rounded-xl">
-          <h2 className="font-semibold text-gray-500 dark:text-neutral-400">
-            {data?.links?.label || def?.links?.label}
-          </h2>
-          <div className="w-full grid grid-cols-2 sm:flex sm:flex-wrap gap-2 sm:gap-3 items-start -mx-4">
-
-            {/* Conditional section : View Resume Btn */}
-            {data?.actions?.show && data?.actions?.resume?.show && (
-              <TestResumeDrawer
-                label={data?.actions?.resume?.label}
-                src={data?.actions?.resume?.url}
-              />
-            )}
-
-            {/* Conditional section : Contact me Btn */}
-            {data?.actions?.show && data?.actions?.contact?.show && (
-              <TestContactDrawer
-                label={data?.actions?.contact?.label}
-                email={data?.actions?.contact?.address}
-              />
-            )}
-
-            {/* The rest of the buttons */}
-            {(data?.links.values || def.links.values).map((link, index) => (
-              <a
-                key={index}
-                className="group flex items-center w-full sm:w-fit font-semibold text-teal-700 dark:text-teal-500 transition-all px-4 py-3 rounded-full hover:bg-teal-50 dark:hover:bg-teal-950"
-                target="_blank"
-                href={link.url}
-                rel="noopener noreferrer"
-                title={"Open " + link.label}
-              >
-                <ArrowUpRightIcon className="w-0 h-0 group-hover:w-5 group-hover:h-5 transition-all duration-500" />
-                <Image
-                  width={20}
-                  height={20}
-                  alt="Link Icon"
-                  src={`/icons/${link.icon}.svg`}
-                  className="w-5 h-5 group-hover:w-0 group-hover:h-0 transition-all duration-500"
-                ></Image>
-                <p className="ml-2">{link.label}</p>
-              </a>
-            ))}
-          </div>
-        </div>
-
-        {/* Experiences section */}
-        {/* <ExperiencesSection data={data} /> */}
-
-        {/* About section */}
-        <div className="text-sm flex flex-col justify-start gap-2 p-4 rounded-xl">
-          <h2 className="font-semibold text-gray-500 dark:text-neutral-400">
-            {data?.about.label || def.about.label}
-          </h2>
-          <p className="text-gray-700 dark:text-neutral-200 text text-left sm:text-justify">
-            {data?.about.value || def.about.value}
-          </p>
-        </div>
-
-        {/* Footer section */}
-        <h2 className="mx-auto text-sm text-center text-gray-400 dark:text-neutral-400 mt-8 pb-6">
-          2024 ©️ {data?.intro.name || def.intro.name}
-        </h2>
+        <IntroSection data={data} />
+        <QuoteSection data={data} />
+        <LinksSection data={data} />
+        <ExperiencesSection data={data} />
+        <AboutSection data={data} />
+        <FooterSection data={data} />
       </main>
     </div>
   );
