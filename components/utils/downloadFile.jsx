0cbc493b9543bd64584204b478257afaeefae8ab
shamimbinzahid-repliq <shamimbinzahid@repliq.dev>
2025-01-29 14:50:09 +0600

    new section components

---
+++ components/utils/downloadFile.jsx
@@ -0,0 +1,18 @@
+import { ArrowDownTrayIcon, ArrowDownIcon } from '@heroicons/react/24/outline';
+
+export function DownloadFile({ download_url }) {
+  return (
+    <a
+      href={download_url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="absolute -top-2 right-0 group flex items-center gap-2 w-fit text-sm text-left font-semibold text-primary-700 dark:text-primary-500 transition-all px-4 py-3 rounded-full hover:bg-primary-50 dark:hover:bg-primary-950"
+    >
+      <div className="flex flex-col">
+        <ArrowDownTrayIcon className="w-5 h-0 transition-all duration-500 group-hover:w-5 group-hover:h-5" />
+        <ArrowDownIcon className="w-5 h-5 transition-all duration-500 group-hover:h-0 stroke-neutral-500 dark:stroke-neutral-400" />
+      </div>
+      Download
+    </a>
+  );
+}
