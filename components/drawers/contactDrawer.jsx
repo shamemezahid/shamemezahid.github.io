e647eaaba3b7beace62728afb0bfa5b57af0398e
shamimbinzahid-repliq <shamimbinzahid@repliq.dev>
2025-02-06 15:09:25 +0600

    render methods called outside component functions

--- components/drawers/contactDrawer.jsx
+++ components/drawers/contactDrawer.jsx
@@ -4,6 +4,19 @@ import { CommonDrawer } from "@/components/drawers/commonDrawer";
 import { ClickToReveal } from "../utils/clickToReveal";
 import { ContactFormSection } from "../sections/contactFormSection";
 
+const renderContactContent = ( data ) => (
+  <>
+    {data?.actions?.contact?.show_email && (
+      <div className="flex flex-row w-full sm:w-fit whitespace-nowrap gap-3 sm:absolute sm:top-1 right-0">
+        <ClickToReveal content={data?.actions?.contact?.address} />
+      </div>
+    )}
+    <div className="w-full h-full flex flex-col text-sm gap-4 overflow-y-auto scrollbar-hide scrollbar-none scrollbar-thumb-rounded-full border-x-0 dark:scrollbar-thumb-neutral-700 scrollbar-thumb-neutral-200 scrollbar-track-transparent mb-8">
+      <ContactFormSection />
+    </div>
+  </>
+);
+
 export function ContactDrawer({ label, data }) {
 
   const ContactIcon = React.forwardRef((props, ref) => (
@@ -19,26 +32,13 @@ export function ContactDrawer({ label, data }) {
   ));
 
   ContactIcon.displayName = "ContactIcon";
-
-  const renderContactContent = () => (
-    <>
-      {data?.actions?.contact?.show_email && (
-        <div className="flex flex-row w-full sm:w-fit whitespace-nowrap gap-3 sm:absolute sm:top-1 right-0">
-          <ClickToReveal content={data?.actions?.contact?.address} />
-        </div>
-      )}
-      <div className="w-full h-full flex flex-col text-sm gap-4 overflow-y-auto scrollbar-hide scrollbar-none scrollbar-thumb-rounded-full border-x-0 dark:scrollbar-thumb-neutral-700 scrollbar-thumb-neutral-200 scrollbar-track-transparent mb-8">
-        <ContactFormSection />
-      </div>
-    </>
-  );
-
+  
   return (
     <CommonDrawer
       triggerIcon={ContactIcon}
       triggerLabel={label}
       triggerTitle="Click to open contact form"
-      renderContent={renderContactContent}
+      renderContent={() => renderContactContent(data)}
       drawerTitle={label}
       drawerSubtitle="Feel free to reach out using the form below."
     />
--- components/drawers/skillsDrawer.jsx
+++ components/drawers/skillsDrawer.jsx
@@ -3,19 +3,19 @@ import { CommonDrawer } from "@/components/drawers/commonDrawer";
 import { WrenchScrewdriverIcon } from "@heroicons/react/24/outline";
 import { SkillsSection } from "../sections/skillsSection";
 
-export function SkillsDrawer({ label, data }) {
-  const renderSkillsContent = () => (
-    <div className="w-full h-full overflow-y-auto scrollbar-hide scrollbar-none scrollbar-thumb-rounded-full border-x-0 dark:scrollbar-thumb-neutral-700 scrollbar-thumb-neutral-200 scrollbar-track-transparent pb-16">
-      <SkillsSection data={data} />
-    </div>
-  );
+const renderSkillsContent = ( data ) => (
+  <div className="w-full h-full overflow-y-auto scrollbar-hide scrollbar-none scrollbar-thumb-rounded-full border-x-0 dark:scrollbar-thumb-neutral-700 scrollbar-thumb-neutral-200 scrollbar-track-transparent pb-16">
+    <SkillsSection data={data} />
+  </div>
+);
 
+export function SkillsDrawer({ label, data }) {
   return (
     <CommonDrawer
       triggerIcon={WrenchScrewdriverIcon}
       triggerLabel={label}
       triggerTitle="Click to view skills"
-      renderContent={renderSkillsContent}
+      renderContent={() => renderSkillsContent(data)}
       drawerTitle={label || "Skills"}
     />
   );
