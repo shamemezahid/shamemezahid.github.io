a7a2e2d0d67fd917212b73fbf598b97d8b2fbc99
shamimbinzahid-repliq <shamimbinzahid@repliq.dev>
2025-02-16 21:17:43 +0600

    Footer links

--- components/sections/FooterSection.jsx
+++ components/sections/FooterSection.jsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function FooterSection({ data }) {
   return (
-    <h2 className="mx-auto text-sm text-center align-baseline text-neutral-400 dark:text-neutral-400 mt-8 pb-6">
-      2024 © {data?.intro?.name}
-    </h2>
+    <div className="flex flex-wrap gap-4 mx-auto text-sm text-center align-baseline items-center text-neutral-400 dark:text-neutral-500 mt-8 pb-6">
+      <h2 className="">2024 © {data?.intro?.name}</h2>
+      {/* <div className="w-1 h-1 rounded-full bg-neutral-200 dark:bg-neutral-700"/> */}
+      {/* <Link href="/404">404</Link> */}
+    </div>
   );
 }
